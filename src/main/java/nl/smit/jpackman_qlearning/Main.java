21739775cad9d5170b0f103dd5e81aedd887bbed
j0rd1Smit <J.Smit-6@student.tudelft.nl>
2017-10-03 11:41:29 +0200

    1st commit

---
+++ src/main/java/nl/smit/jpackman_qlearning/Main.java
@@ -0,0 +1,12 @@
+package nl.smit.jpackman_qlearning;
+
+/**
+ * [Class explanation]
+ *
+ * @author Jordi Smit, 3-10-2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
